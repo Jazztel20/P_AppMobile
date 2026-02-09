7685d3e6f65bca670b71e2e0eca550f2545904a7
Jazztel20 <pb58unc@eduvaud.ch>
2026-02-09 10:20:49 +0100

    update(migrations): mise à jour de la migration de P_PassionLecture

--- P_PassionLecture/Backend/p-web-back-end/database/migrations/4_create_books_table.ts
+++ P_PassionLecture/Backend/p-web-back-end/database/migrations/4_create_books_table.ts
@@ -2,47 +2,44 @@ import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
   protected tableName = 'books'
-  schema: any
 
-  async up() {
+  public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
       table.string('title').notNullable()
       table.string('author').nullable()
-      // Utilisation de MEDIUMBLOB pour les fichiers binaires selon le document technique
+
       table.specificType('epub_file', 'MEDIUMBLOB').notNullable()
       table.specificType('cover_image', 'MEDIUMBLOB').nullable()
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
 
-      //Déclaration des foreign keys 
       table
         .integer('category_id')
         .unsigned()
+        .nullable()
         .references('id')
-        .inTable('categories')  //quelle référence pour cerner en majuscule ou minuscule?
-        .onDelete('SET NULL') //malgré suppression de livres/commentaires la catégorie est permanente 
+        .inTable('categories')
+        .onDelete('SET NULL')
 
       table
         .integer('writer_id')
         .unsigned()
+        .nullable()
         .references('id')
         .inTable('writers')
-        .onDelete('SET NULL') // a checker ultérieurement (logique)
+        .onDelete('SET NULL')
 
       table
         .integer('user_id')
         .unsigned()
         .references('id')
         .inTable('users')
-        .onDelete('CASCADE') // si on supprime le user on supprime ses livres
+        .onDelete('CASCADE')
 
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
+      table.timestamps(true)
     })
   }
 
-  async down() {
+  public async down () {
     this.schema.dropTable(this.tableName)
   }
 }
